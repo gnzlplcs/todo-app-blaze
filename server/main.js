ff94d6d77be91213dbbbce1ef04439d7365e115b
gnzlplcs <palaciosgm@outlook.com>
2023-03-25 15:30:21 -0500

    🚧 initialize tasks collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "../imports/ui/api/TasksCollection";
+
+const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (TasksCollection.find().count() === 0) {
+    [
+      "first task",
+      "second task",
+      "third task",
+      "fourth task",
+      "fifth task",
+    ].forEach(insertTask);
+  }
 });
