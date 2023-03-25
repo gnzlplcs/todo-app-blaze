36045cf42ef8b0261a3a4ceec7e7f42b26b16c8f
gnzlplcs <palaciosgm@outlook.com>
2023-03-25 18:29:54 -0500

    📢 add submit listener

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,26 @@ import "./App.html";
 
 Template.mainContainer.helpers({
   tasks() {
-    return TasksCollection.find({})
-  }
-})
+    return TasksCollection.find({});
+  },
+});
+
+Template.form.events({
+  "submit .task-form"(event) {
+    // prevent default browser form submit
+    event.preventDefault();
+
+    // get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert a task into the collection
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // clear form
+    target.text.value = "";
+  },
+});
