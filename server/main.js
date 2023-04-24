d305d7373a1977e592581df6297adfa82d49857e
gnzlplcs <palaciosgm@outlook.com>
2023-04-24 17:22:32 -0500

    :construction: create user account

--- server/main.js
+++ server/main.js
@@ -1,10 +1,22 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import { TasksCollection } from "../imports/api/TasksCollection";
 
 const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
 
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
+
 Meteor.startup(() => {
   // code to run on server at startup
+
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if (TasksCollection.find().count() === 0) {
     [
       "first task",
