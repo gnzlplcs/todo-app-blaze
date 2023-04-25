c0a8aa704aecdd24d17743a1935a1ffdb09d4a36
gnzlplcs <palaciosgm@outlook.com>
2023-04-24 22:24:41 -0500

    :technologist: server startup

--- server/main.js
+++ server/main.js
@@ -2,7 +2,12 @@ import { Meteor } from "meteor/meteor";
 import { Accounts } from "meteor/accounts-base";
 import { TasksCollection } from "../imports/api/TasksCollection";
 
-const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createAt: new Date(),
+  });
 
 const SEED_USERNAME = "meteorite";
 const SEED_PASSWORD = "password";
@@ -17,6 +22,8 @@ Meteor.startup(() => {
     });
   }
 
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
   if (TasksCollection.find().count() === 0) {
     [
       "first task",
@@ -26,6 +33,6 @@ Meteor.startup(() => {
       "fifth task",
       "sixth task",
       "seventh task",
-    ].forEach(insertTask);
+    ].forEach(taskText => insertTask(taskText, user));
   }
 });
